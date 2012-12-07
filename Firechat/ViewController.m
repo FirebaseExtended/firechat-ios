d89dcc58f2dfa84ff92e4735ce4da59d75a07811
Anant Narayanan <anant@kix.in>
2012-12-06 17:54:24 -0800

    Slide up view on keyboard appearance

--- Firechat/ViewController.m
+++ Firechat/ViewController.m
@@ -14,6 +14,42 @@
 
 @implementation ViewController
 
+- (void)keyboardWillShow:(NSNotification*)notification
+{
+    [self moveView:[notification userInfo] up:YES];
+}
+
+- (void)keyboardWillHide:(NSNotification*)notification
+{
+    [self moveView:[notification userInfo] up:NO];
+}
+
+- (void)moveView:(NSDictionary*)userInfo up:(BOOL)up
+{
+    CGRect keyboardEndFrame;
+    [[userInfo objectForKey:UIKeyboardFrameEndUserInfoKey]
+        getValue:&keyboardEndFrame];
+    
+    UIViewAnimationCurve animationCurve;
+    [[userInfo objectForKey:UIKeyboardAnimationCurveUserInfoKey]
+        getValue:&animationCurve];
+    
+    NSTimeInterval animationDuration;
+    [[userInfo objectForKey:UIKeyboardAnimationDurationUserInfoKey]
+        getValue:&animationDuration];
+    
+    [UIView beginAnimations:nil context:nil];
+    [UIView setAnimationBeginsFromCurrentState:YES];
+    [UIView setAnimationDuration:animationDuration];
+    [UIView setAnimationCurve:animationCurve];
+
+    CGRect keyboardFrame = [self.view convertRect:keyboardEndFrame toView:nil];
+    int y = keyboardFrame.size.height * (up ? -1 : 1);
+    self.view.frame = CGRectOffset(self.view.frame, 0, y);
+
+    [UIView commitAnimations];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -24,6 +60,26 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [[NSNotificationCenter defaultCenter]
+        addObserver:self selector:@selector(keyboardWillShow:)
+        name:UIKeyboardWillShowNotification object:nil];
+
+    [[NSNotificationCenter defaultCenter]
+        addObserver:self selector:@selector(keyboardWillHide:)
+        name:UIKeyboardWillHideNotification object:nil];
+}
+
+- (void)viewWillDisappear:(BOOL)animated
+{
+    [[NSNotificationCenter defaultCenter]
+        removeObserver:self name:UIKeyboardWillShowNotification object:nil];
+    
+    [[NSNotificationCenter defaultCenter]
+        removeObserver:self name:UIKeyboardWillHideNotification object:nil];
 }
 
 @end
